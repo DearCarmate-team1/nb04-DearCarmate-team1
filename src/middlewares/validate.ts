ba5eb1c570e45d80cf947d1d1ec92bd71f87d57b
오연진 <yonjin.oh@gmail.com>
2025-10-07 15:53:33 +0900

    feat: validate 미들웨어 검증 대상 명확하게 수정 #8
    해당 이슈에 자세하게 작성해두었습니다.

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,17 +1,14 @@
-import { ZodObject } from 'zod';
+import { ZodObject, ZodError } from 'zod';
 import type { Request, Response, NextFunction } from 'express';
 
 export const validate =
-  (schema: ZodObject) => (req: Request, res: Response, next: NextFunction) => {
+  (schema: ZodObject, part: 'body' | 'query' | 'params') =>
+  (req: Request, res: Response, next: NextFunction) => {
     try {
-      // 이전 코드: schema.parse({ body: req.body, ... }) -> req.body를 객체로 감싸서 검증했기 때문에 오류 발생
-      // 수정 후: req.body를 직접 검증하도록 변경
-      schema.parse(req.body);
+      const validatedData = schema.parse(req[part]);
+      Object.assign(req[part], validatedData);
       next();
-    } catch (e: any) {
-      return res.status(400).json({
-        message: 'Validation Error',
-        errors: e.errors,
-      });
+    } catch (e) {
+      next(e);
     }
   };
