8fad78ff0eab6eab8db746fef2422ded28c154ea
Jiheon <jiheon0621@naver.com>
2025-10-10 19:05:54 +0900

    feat: 유저 CRUD API 유효성검사 제외

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,11 @@ import cors from 'cors';
 
 import companyRoute from './routes/company-route.js';
 import userRoute from './routes/user-route.js';
+<<<<<<< HEAD
 import authRoute from './routes/auth-route.js';
 import { errorHandler } from './middlewares/error-handler.js';
+=======
+>>>>>>> ee42218 (feat: 유저 CRUD API 유효성검사 제외)
 
 const app = express();
 const port = 3001;
@@ -22,12 +25,16 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
+
 app.use('/companies', companyRoute);
 app.use('/users', userRoute);
+<<<<<<< HEAD
 app.use('/auth', authRoute);
 
 // 에러 핸들러 미들웨어는 가장 마지막에 등록
 app.use(errorHandler);
+=======
+>>>>>>> ee42218 (feat: 유저 CRUD API 유효성검사 제외)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- src/repositories/company-repository.ts
+++ src/repositories/company-repository.ts
@@ -233,7 +233,27 @@ const companyRepository = {
   async delete(companyId: number) {
     await prisma.company.delete({ where: { id: companyId } });
   },
+<<<<<<< HEAD
 >>>>>>> 3cd660d (refactor: 회사 관련 주석 추가)
+=======
+<<<<<<< HEAD
+=======
+
+  // 유저 회원가입 시, 회사명과 인증코드를 검증하기 위해 추가
+  async findByNameAndAuthCode(name: string, authCode: string) {
+    return prisma.company.findFirst({
+      where: {
+        AND: [{ name }, { authCode }],
+      },
+    });
+  },
+
+  async update() {},
+  async delete() {},
+  async getAll() {},
+  async getById() {},
+>>>>>>> ccd47ad (feat: 유저 CRUD API 유효성검사 제외)
+>>>>>>> ee42218 (feat: 유저 CRUD API 유효성검사 제외)
 };
 
 export default companyRepository;
