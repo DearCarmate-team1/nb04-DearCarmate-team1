0753115c63937fe2b97867a5fcd27244e57e14d6
오연진 <yonjin.oh@gmail.com>
2025-10-11 01:35:02 +0900

    feat: cors install 및 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+
 import companyRoute from './routes/company-route.js';
 
 const app = express();
 const port = 3001;
+
+const allowedOrigins = ['http://localhost:3000']
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+}
+
+app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use('/companies', companyRoute);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
