f7f948cb6df23cba7e54f7be301d7a240426502f
jhhong0571 <105486482+jhhong0571@users.noreply.github.com>
2025-10-14 16:36:32 +0900

    고객 라우터 연결 추가

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import companyRoute from './routes/company-route.js';
 import userRoute from './routes/user-route.js';
 import authRoute from './routes/auth-route.js';
 import { errorHandler } from './middlewares/error-handler.js';
+import customerRoutes from './routes/customerRoutes';
 
 const app = express();
 const port = 3001;
@@ -26,6 +27,7 @@ app.use(cors(corsOptions));
 app.use('/companies', companyRoute);
 app.use('/users', userRoute);
 app.use('/auth', authRoute);
+app.use('/customers', customerRoutes);
 
 // 에러 핸들러 미들웨어는 가장 마지막에 등록
 app.use(errorHandler);
