ae7da17ad3fe29b625b4c3b308afb58551404020
jhhong0571 <105486482+jhhong0571@users.noreply.github.com>
2025-10-23 16:50:24 +0900

    develop 최신화 및 계약서 업로드 코드 수정

--- src/routes/contract-document-route.ts
+++ src/routes/contract-document-route.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { ContractDocumentController } from '../controllers/contract-document-controller.js';
+import { contractDocumentController as controller } from '../controllers/contract-document-controller.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { uploadDocument } from '../configs/multer.js';
 import asyncHandler from '../configs/async-handler.js';
 
 const router = Router();
-const controller = new ContractDocumentController();
+
 
 router.get("/", authenticate, asyncHandler(controller.list.bind(controller)));
 
